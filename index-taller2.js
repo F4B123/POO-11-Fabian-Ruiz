245948f5839f9aec52c1b6c1f027e6f087050f69
Fabian Ruiz <40047900+F4B123@users.noreply.github.com>
2020-09-09 13:47:57 -0500

    Add files via upload

---
+++ index-taller2.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById('lienzo');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+canvas.style.backgroundColor = '#eeff00';
+
+//agregar un Contexto
+
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'orangered';
+ctx.fillRect(100, 100, 400, 400);
+
+
+ctx.fillStyle = '#ff4500';
+ctx.fillRect(600, 100, 400, 400);
+
+
+
